0dd12727c0ff19e8d4886761f029fdde9da2591e
Max Cobb <maxxc@mac.com>
2021-11-29 10:26:01 -0800

    release 3.5.2

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
         .library(
             name: "AgoraRtcKit",
             targets: [
-                "AgoraAIDenoiseExtension", "AgoraCore", "AgoraDav1dExtension", "AgoraFDExtension", "Agorafdkaac",
+                "AgoraAIDenoiseExtension", "AgoraCIExtension", "AgoraCore", "AgoraDav1dExtension", "AgoraFDExtension", "Agorafdkaac",
                 "Agoraffmpeg", "AgoraJNDExtension", "AgoraRtcKit", "AgoraSoundTouch",
                 "AgoraVideoSegmentationExtension", "av1"
             ]
@@ -20,58 +20,63 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "AgoraAIDenoiseExtension",
-            url: "https://github.com/AgoraIO/AgoraRtcEngine_macOS/releases/download/3.5.1/AgoraAIDenoiseExtension.xcframework.zip",
-            checksum: "88e818a27d5f20a0f1d6706cadd39e0c177757816be8285a3f9a4ee1ded52372"
+            url: "https://github.com/AgoraIO/AgoraRtcEngine_macOS/releases/download/3.5.2/AgoraAIDenoiseExtension.xcframework.zip",
+            checksum: "ea7e8c63a09bd6692c28d1583eadeb84ac3cca1ce0e64923998717fbb6f069dc"
+        ),
+        .binaryTarget(
+            name: "AgoraCIExtension",
+            url: "https://github.com/AgoraIO/AgoraRtcEngine_macOS/releases/download/3.5.2/AgoraCIExtension.xcframework.zip",
+            checksum: "f76436359ed651ba4506d1bc4ae24ac4d342c4fcb8f68825f078f2fd70fd03ae"
         ),
         .binaryTarget(
             name: "AgoraCore",
-            url: "https://github.com/AgoraIO/AgoraRtcEngine_macOS/releases/download/3.5.1/AgoraCore.xcframework.zip",
-            checksum: "a77771f142f1dd392c31be6e0edd8f4db5fc7aacf34383a7006439ddba930e4e"
+            url: "https://github.com/AgoraIO/AgoraRtcEngine_macOS/releases/download/3.5.2/AgoraCore.xcframework.zip",
+            checksum: "6e645e3254040dd24b6570516565a36bc369c55e035e6c7ab479a53dd96079b2"
         ),
         .binaryTarget(
             name: "AgoraDav1dExtension",
-            url: "https://github.com/AgoraIO/AgoraRtcEngine_macOS/releases/download/3.5.1/AgoraDav1dExtension.xcframework.zip",
-            checksum: "82363168fc7ec50525b0200133d9b540b7c7096b8c489d24b425e16d8a095486"
+            url: "https://github.com/AgoraIO/AgoraRtcEngine_macOS/releases/download/3.5.2/AgoraDav1dExtension.xcframework.zip",
+            checksum: "773a6dc1aa7fcb4a6310832f74b885a26a5a02e8ab58f917feb26c56d2df4dd5"
         ),
         .binaryTarget(
             name: "AgoraFDExtension",
-            url: "https://github.com/AgoraIO/AgoraRtcEngine_macOS/releases/download/3.5.1/AgoraFDExtension.xcframework.zip",
-            checksum: "aaa8285c8c7b58833935fddfc28e413b735dc6fcdca0df09e3e18f97b3c991fa"
+            url: "https://github.com/AgoraIO/AgoraRtcEngine_macOS/releases/download/3.5.2/AgoraFDExtension.xcframework.zip",
+            checksum: "9cd7a29624637f79aecf52d2c8e4871ed00ff215470ff8d8c4cbd80eabe08c03"
         ),
         .binaryTarget(
             name: "Agorafdkaac",
-            url: "https://github.com/AgoraIO/AgoraRtcEngine_macOS/releases/download/3.5.1/Agorafdkaac.xcframework.zip",
-            checksum: "10e1eb50543c4ae0c67aa43e3081671332508fbed4db7b8f2a8a41c7e800eca6"
+            url: "https://github.com/AgoraIO/AgoraRtcEngine_macOS/releases/download/3.5.2/Agorafdkaac.xcframework.zip",
+            checksum: "e9a0d009000462bd0df6fcd0070e7613f49cb1bfad23ab1b45852cde533c2491"
         ),
         .binaryTarget(
             name: "Agoraffmpeg",
-            url: "https://github.com/AgoraIO/AgoraRtcEngine_macOS/releases/download/3.5.1/Agoraffmpeg.xcframework.zip",
-            checksum: "4d21009a230443f0eb038e8b77bdfead941ccc8778367b2a4145fa71facc47d1"
+            url: "https://github.com/AgoraIO/AgoraRtcEngine_macOS/releases/download/3.5.2/Agoraffmpeg.xcframework.zip",
+            checksum: "559e481d299c43f62b525af12d656f1cab86228fac4fc518b4927bbcffc88f93"
         ),
         .binaryTarget(
             name: "AgoraJNDExtension",
-            url: "https://github.com/AgoraIO/AgoraRtcEngine_macOS/releases/download/3.5.1/AgoraJNDExtension.xcframework.zip",
-            checksum: "987686e254ac8ef86f92a0b55158e8c38569acfa47d5b90f0f1d3974f85267ee"
+            url: "https://github.com/AgoraIO/AgoraRtcEngine_macOS/releases/download/3.5.2/AgoraJNDExtension.xcframework.zip",
+            checksum: "0ef21f5767055e4d716152e9e994d6fe1c00fbc3729918759b4970f77ae38a97"
         ),
         .binaryTarget(
             name: "AgoraRtcKit",
-            url: "https://github.com/AgoraIO/AgoraRtcEngine_macOS/releases/download/3.5.1/AgoraRtcKit.xcframework.zip",
-            checksum: "c2a1ad76328b66db0782f2fca2410cc6e2ba6eb0879b65f4385ac8847994d2b5"
+            url: "https://github.com/AgoraIO/AgoraRtcEngine_macOS/releases/download/3.5.2/AgoraRtcKit.xcframework.zip",
+            checksum: "e17713b2830a7c3f73ca224f090901d6d983ce715bad93593de7c5271d6b6d99"
         ),
         .binaryTarget(
             name: "AgoraSoundTouch",
-            url: "https://github.com/AgoraIO/AgoraRtcEngine_macOS/releases/download/3.5.1/AgoraSoundTouch.xcframework.zip",
-            checksum: "525c2dacaae8cbc78ba07a970e3cd752ae268f1a71145ef762100e78fe228924"
+            url: "https://github.com/AgoraIO/AgoraRtcEngine_macOS/releases/download/3.5.2/AgoraSoundTouch.xcframework.zip",
+            checksum: "49a6b1f72552fa8e6543b62626878fd212daf26c3892d943cc3c71951c177e14"
         ),
         .binaryTarget(
             name: "AgoraVideoSegmentationExtension",
-            url: "https://github.com/AgoraIO/AgoraRtcEngine_macOS/releases/download/3.5.1/AgoraVideoSegmentationExtension.xcframework.zip",
-            checksum: "602a6729d0831a8e00c5d685b857be488bcdeba49e0c32acbe5335aa170406a0"
+            url: "https://github.com/AgoraIO/AgoraRtcEngine_macOS/releases/download/3.5.2/AgoraVideoSegmentationExtension.xcframework.zip",
+            checksum: "c48066d29d01de0943b0fa8dfcef0cee44d16ef054d56bbeaea8e0d5841879ad"
         ),
         .binaryTarget(
             name: "av1",
-            url: "https://github.com/AgoraIO/AgoraRtcEngine_macOS/releases/download/3.5.1/av1.xcframework.zip",
-            checksum: "ced5761afb060c6731499f5a2c8c80b7a6e25e1958de5990467378d8bd8f97f4"
+            url: "https://github.com/AgoraIO/AgoraRtcEngine_macOS/releases/download/3.5.2/av1.xcframework.zip",
+            checksum: "cdfecda09eaf144f21b5876321b5b7bddad9571d4635ecfdd4dcf8a1c7a32c1e"
         )
     ]
 )
